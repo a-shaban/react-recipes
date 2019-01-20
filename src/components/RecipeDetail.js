87a11a37cc8078cefdceb61025b77e31cbca502c
Abdo <abdosalah3105@gmail.com>
2019-01-20 17:40:28 +1100

    Making the website interactive

--- src/components/RecipeDetail.js
+++ src/components/RecipeDetail.js
@@ -1,9 +1,43 @@
 import React from 'react';
 
-const RecipeDetail = (props) => (
-   <div style={props.style}>
-      <h1>RecipeDetail</h1>
-   </div>
-);
+const RecipeDetail = (props) => {
+
+   if (!props.recipe) {
+      return (
+         <p style={props.style}>
+            Please select a recipe to see the details
+         </p>
+      )
+   }
+   return (
+      <div style={props.style}>
+
+         <h2>{props.recipe.name}</h2>
+         <img src={props.recipe.image} />
+         <div>
+            <span>{props.recipe.category}</span>
+            <span>{props.recipe.calories}</span>
+         </div>
+
+         <h3>Ingredients</h3>
+         <ul>
+            {props.recipe.ingredients.map(ingredients => (
+               <li key={ingredients} >
+                  {ingredients}
+               </li>
+            ))}
+         </ul>
+
+         <h3>Steps</h3>
+         <ul>
+            {props.recipe.steps.map(steps => (
+               <li key={steps} >
+                  {steps}
+               </li>
+            ))}
+         </ul>
+      </div>
+   )
+};
 
 export default RecipeDetail;
--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -4,15 +4,15 @@ const RecipeList = (props) => (
    <div style={props.style}>
       <h1>Recipes</h1>
       <ul>
-         <li>
-            <span> REC </span>
-         </li>
-         <li>
-            <span> REC </span>
-         </li>
-         <li>
-            <span> REC 3 </span>
-         </li>
+         {props.recipes.map(recipe => (
+            <li
+               key={recipe.id}
+               onClick={() => props.onClick(recipe.id)}>
+               <span> {recipe.name} </span>
+               <br />
+               <span> {recipe.category} </span>
+            </li>
+         ))}
       </ul>
    </div>
 );

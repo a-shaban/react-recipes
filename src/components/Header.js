47ff696f2b52fd679eff4261a171e32b277ad06e
Abdo <abdosalah3105@gmail.com>
2019-01-23 22:28:58 +1100

    Adding eslint and Aribnb code style

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,10 +2,14 @@ import React from 'react';
 import { Twemoji } from 'react-emoji-render';
 
 const Header = () => (
-   <header className="px4" >
+  <header className="px4">
 
-      <h1 className="h1"> <Twemoji text=":fork_and_knife:" />MyRecipes</h1>
-   </header>
+    <h1 className="h1">
+      {' '}
+      <Twemoji text=":fork_and_knife:" />
+MyRecipes
+    </h1>
+  </header>
 );
 
 export default Header;

e6727662c9bc3b421233b345dc5524f519c68aa7
ZeeshanMujtaba <s3614080@student.rmit.edu.au>
2019-03-03 18:47:51 +1100

    Add Footer

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,16 @@ import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Home from './Home';
 import Favorites from './Favorites';
-// import Header from './Header';
+import Header from './Header';
+import Footer from './Footer';
 
 const App = () => (
   <BrowserRouter>
     <main>
-      {/* <Header /> */}
+      <Header /> 
       <Route exact path="/" component={Home} />
       <Route path="/favorites" component={Favorites} />
+      <Footer /> 
     </main>
   </BrowserRouter>
 );
---
+++ src/components/Footer.js
@@ -0,0 +1,50 @@
+import React from 'react';
+
+
+const Footer = () => (
+
+	<footer className="page-footer font-small footer">
+
+		<div className="container footer-copyright text-center ">
+
+			<div className="">
+				<div className="col-md-2 mb-3">
+					<h5 class="text-uppercase font-weight-bold">
+						<a className=" active" href="/Home" >Home</a>
+					</h5>
+				</div>
+				<div className="col-md-2 mb-3">
+					<h5 class="text-uppercase font-weight-bold">
+						<a href="/Home" >About</a>
+					</h5>
+				</div>
+				<div className="col-md-2 mb-3">
+					<h5 class="text-uppercase font-weight-bold">
+						<a href="/Home" >FAQ</a>
+					</h5>
+				</div>
+				<div className="col-md-2 mb-3">
+					<h5 class="text-uppercase font-weight-bold">
+						<a href="/Home" >Get Started</a>
+					</h5>
+				</div>
+				<div className="col-md-2 mb-3">
+					<h5 class="text-uppercase font-weight-bold">
+						<a href="/Home" >Videos</a>
+					</h5>
+				</div>
+				<div className="col-md-2 mb-3">
+					<h5 class="text-uppercase font-weight-bold">
+						<a href="/Home" >Contact Us</a>
+					</h5>
+				</div>
+			</div>
+		</div>
+
+		<div className="footer-copyright text-center py-3">© 2018 Copyright:
+		</div>
+
+	</footer>
+);
+
+export default Footer;
